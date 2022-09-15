f2bd39224b7a56ac55c03503a8a0f47aaf6a941d
Romskul <inaki.martin.c@gmail.com>
2022-09-15 18:25:51 +0200

    añadida directiva ngClass a texto bindeado de "cargo"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  title = 'Registro de Usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre:string = "";
+  apellido:string = "";
+  cargo:string = "";
+  entradas:Array<{titulo:string}>;
+  // entradas:any;
+
+  constructor() { 
+
+    this.entradas = [
+
+      {titulo: "Python cada día mas presente"},
+      {titulo: "Java presente desde hace mas de 20 años"},
+      {titulo: "Javascript cada día mas funcional"},
+      {titulo: "Kotlin potencia para tus apps"},
+      {titulo: "¿Dónde quedó Pascal?"},
+
+    ];
+
+  }
+
+  registrarUsuario(){
+
+    this.registrado = true;
+    this.mensaje = "Usuario registrado con exito";
+  }
 }
